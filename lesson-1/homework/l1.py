c1899ef1be6b27a0bae8882afcd7a379c10ea7e3
Bobomurod2002 <rahmatovbobomurod859@gmail.com>
2025-08-13 00:41:28 +0500

    Create l1.py
    Lesson-1

---
+++ lesson-1/homework/l1.py
@@ -0,0 +1,27 @@
+a = 7
+p = 4 * a
+S = a * a
+print(S)
+print(p)
+
+
+import math 
+d = float(input("Doiraning diametrini kiriting: "))
+
+L = d * math.pi 
+print(L)    
+
+
+a = int(input("1-sonni kiriting:  "))
+b = int(input("2-soonni kiritng:  "))
+ortachasi = (a + b)/2
+print(ortachasi)
+
+
+a = 15
+b= 10
+c = a + b
+d = a * b
+e = a ** 2
+f = b ** 2
+print(c,  d,  e,  f)
